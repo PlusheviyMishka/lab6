4b3c069b09ca99bc4f21c63765d4c452877c4ec6
Mikhail <plusheviymishkakorotyshka@gmail.com>
2025-12-10 16:47:56 +0300

    'Initial'

---
+++ build/CMakeFiles/lab6_exe.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab6_exe.dir/main.cpp.obj"
+  "CMakeFiles/lab6_exe.dir/main.cpp.obj.d"
+  "lab6_exe.exe"
+  "lab6_exe.exe.manifest"
+  "lab6_exe.pdb"
+  "liblab6_exe.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab6_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
